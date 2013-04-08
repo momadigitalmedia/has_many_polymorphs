4b25dd839ff9edac5efb86c0f8a94ea9c87afb01
Anay Kamat <anay@presentsoft.co.in>
2013-04-08 12:35:54 +0530

    anay: adding support to concat new items using  "<<" operator on the association

--- lib/has_many_polymorphs/association.rb
+++ lib/has_many_polymorphs/association.rb
@@ -15,6 +15,42 @@ module ActiveRecord #:nodoc:
         association_scope.where(reflection.additional_conditions)
       end
 
+
+      def concat(*records)
+        return if records.empty?
+
+        if reflection.options[:skip_duplicates]
+          _logger_debug "Loading instances for polymorphic duplicate push check; use :skip_duplicates => false and perhaps a database constraint to avoid this possible performance issue"
+          load_target
+        end
+
+        reflection.klass.transaction do
+          records.flatten.each do |record|
+            if owner.new_record? or not record.respond_to?(:new_record?) or record.new_record?
+              raise PolymorphicError, "You can't associate unsaved records."
+            end
+            next if reflection.options[:skip_duplicates] and target.include? record
+            owner.send(reflection.options[:through]).create(construct_join_attributes(record))
+            @target << record if loaded?
+          end
+        end
+
+        self
+      end
+
+      private
+
+      def construct_join_attributes(record)
+
+        join_attributes = {
+            record.association(reflection.options[:through]).reflection.foreign_key => record.send(record.association(reflection.options[:through]).reflection.association_primary_key(reflection.klass))
+        }
+
+        join_attributes[record.association(reflection.options[:through]).reflection.type] = record.class.base_class.name
+
+        join_attributes
+      end
+
     end
 
   end
